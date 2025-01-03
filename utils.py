271e944f99d387f151fa0dc1d7589170edbc06ab
AmanDevelops <amanpal2072004@gmail.com>
2025-01-03 21:05:35 +0530

    implement fetch function to retrieve and display open hackathons data

--- utils.py
+++ utils.py
@@ -1,5 +1,13 @@
 import requests
+import json
 
-def fetch(number=10):
-    pass
 
+import requests
+
+uuid = "YOUR_UNIQUE_ID"
+
+response = requests.get(f'https://devfolio.co/_next/data/{uuid}/hackathons.json')
+
+data = response.json()
+
+print(json.dumps(data['pageProps']['dehydratedState']['queries'][0]['state']['data']['open_hackathons'][2], indent=4))
